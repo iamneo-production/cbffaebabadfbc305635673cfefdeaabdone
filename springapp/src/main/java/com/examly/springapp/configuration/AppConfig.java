f726b738c85937a1de77f65a1cfed4397ca3423d
Iamneo <techadmin@iamneo.ai>
2023-09-30 18:52:59 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
+++ springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
@@ -1,6 +1,21 @@
 package com.examly.springapp.configuration;
-// @Configuration
-// @ComponentScan(basePackages = "com.examly.springapp")
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.examly.springapp.model.Student;
+
+@Configuration
+@ComponentScan(basePackages = "com.examly.springapp")
 public class AppConfig {
-    // Additional configurations if needed
+
+    // Define a bean for the Student class
+    @Bean
+    public Student student() {
+        return new Student();
+    }
+
+    // You can define more beans and configurations as needed for your project
+
 }
