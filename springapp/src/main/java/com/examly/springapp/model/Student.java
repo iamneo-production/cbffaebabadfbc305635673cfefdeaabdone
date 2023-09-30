e5d4fe4eb03c5e5232a11d1b0671b98e5d835975
Iamneo <techadmin@iamneo.ai>
2023-09-30 17:57:39 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/model/Student.java
+++ springapp/src/main/java/com/examly/springapp/model/Student.java
@@ -1,1 +1,13 @@
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    void setStudentId(int i){studentId=i;}
+    void setFirstName(String i){firstName=i;}
+    void setLastName(String i){lastName=i;}
+
+    int getStudentId(){return studentId;}
+    String getFirstName(){return firstName;}
+    String getLastName(){return lastName;}
+}
 
