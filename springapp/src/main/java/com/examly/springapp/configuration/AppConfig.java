a0b86b3796d574e8bcd8eb7f3f19841a81629f08
Iamneo <techadmin@iamneo.ai>
2023-09-30 18:23:04 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
+++ springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
@@ -1,6 +1,6 @@
 
-@Configuration
-@ComponentScan(basePackages = "com.examly.springapp")
+// @Configuration
+// @ComponentScan(basePackages = "com.examly.springapp")
 public class AppConfig {
     // Additional configurations if needed
 }
